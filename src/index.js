3054013a8b67ce55e26a1b2750a55e0f62ae4d50
Aniruddh Gopale <anigopale@gmail.com>
2018-02-10 14:17:14 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import 'semantic-ui-css/semantic.min.css';
+
+import './index.css';
+import App from './App';
+import reducers from './root-reducer';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
+registerServiceWorker();

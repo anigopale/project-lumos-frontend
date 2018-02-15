e408d829a7359ff6f67d4bd3d9c9415057a85757
Aniruddh Gopale <anigopale@gmail.com>
2018-02-15 13:29:06 +0530

    change file-tree structure

--- src/scenes/domain-page/index.js
+++ src/scenes/domain-page/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from '../../common-components/navbar';
-import Domains from './components/domains';
+import Domains from '../../navigation/domains';
 
 export default class DomainPage extends Component {
   render() {
--- src/scenes/language-page/index.js
+++ src/scenes/language-page/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from '../../common-components/navbar';
-import Languages from './components/languages';
+import Languages from '../../navigation/languages';
 
 export default class LanguagePage extends Component {
   render() {

a274650887b0457676f7559eec07fc570e15ab5d
Aniruddh Gopale <anigopale@gmail.com>
2018-02-15 22:17:09 +0530

    render soft skills

--- src/navigation/soft-skills/index.js
+++ src/navigation/soft-skills/index.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Container, Segment, Grid, Divider, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import { fetchSoftSkills } from './actions';
 
 class SoftSkills extends Component {
   componentDidMount() {
     this.props.fetchSoftSkills();
   }
+  renderSoftSkills() {
+    return this.props.softskills.map((skill) => {
+      return (
+        <Grid.Column>
+          <Segment
+            textAlign='center'
+            size='massive'
+            color='teal'
+            inverted
+            padded
+            basic
+            as={Link}
+            to={`/courses/${skill.slug}/0`}
+            >
+            {skill.domain_name}
+          </Segment>
+        </Grid.Column>
+      )
+    })
+  }
+
+
   render() {
     return (
       <div>
-
+        <Container>
+          <Segment basic>
+            <Divider hidden />
+            <Header as='h1' textAlign='center'>
+              <Header sub>Browse courses by</Header>
+              Soft Skills
+            </Header>
+            <Divider />
+            <Grid columns={3} stretched stackable>
+              {this.renderSoftSkills()}
+            </Grid>
+          </Segment>
+        </Container>
       </div>
     )
   }

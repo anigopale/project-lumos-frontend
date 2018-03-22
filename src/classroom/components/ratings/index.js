2dadcbcee157a92e909fec6a032f5b1a635c1ab6
Aniruddh Gopale <anigopale@gmail.com>
2018-03-22 14:58:45 +0530

    render fetched ratings in classroom page

--- src/classroom/components/ratings/index.js
+++ src/classroom/components/ratings/index.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import { Segment, Progress, Label } from 'semantic-ui-react';
 
 export default class Ratings extends Component {
+  renderRatings() {
+    let flag = true;
+    let { ratings } = this.props;
+      Object.keys(ratings).map((key => {
+        if(ratings[key] === 0)
+          flag = false;
+      }));
+      if(flag) {
+        return Object.keys(ratings).map((key => {
+          let percent = ratings[key]*20;
+          return (
+            <div>
+              {key}: {ratings[key]} <Progress percent={percent} size='small' />
+            </div>
+          )
+        }))
+    }
+    return (
+      <div>Be the first one to rate this!</div>
+    )
+  }
+
   render() {
     return (
       <div>
-        Attribute name:<Progress percent='20' size='small' />
-        Attribute name:<Progress percent='20' size='small' />
-        Attribute name:<Progress percent='20' size='small' />
-        Attribute name:<Progress percent='20' size='small' />
+        {this.renderRatings()}
       </div>
     )
   }
